afbb3bbb2fb3bfff4f23166d119df9ea981fd943
DariaDubnikova <d.dasha20010513@gmail.com>
2020-03-22 17:21:17 +0300

    сделана lab2

---
+++ lab2/print_args/print_args/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace print_args
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Number of arguments: " + args.Length);
+                Console.Write("Arguments: ");
+                foreach (string argument in args)
+                {
+                    Console.Write(argument + " ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No parameters were specified!");
+            }
+        }
+    }
+}
---
+++ lab2/remove_duplicates/remove_duplicates/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace remove_duplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                Console.WriteLine(args[0].Distinct().ToArray());
+            }
+            else
+            {
+                Console.WriteLine("Incorrect number of arguments!\nUsage remove_duplicates.exe <input string>");
+            }
+        }
+    }
+}
